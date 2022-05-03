dd7e578c6a3e4491c80ba031da7b2e1bf4245c52
octrung <t.nguyenvo@eagles.oc.edu>
2022-05-03 18:40:04 -0500

    better flip animation

--- components/Card.js
+++ components/Card.js
@@ -7,16 +7,16 @@ const Card = ({ card, index }) => {
   let delay = `calc(${index} * 200ms + ${baseDelay})`
 
   return (
-    <div id='card' className={containerStyle}>
-      <img 
-        key={card.id} 
-        src={card.images.large} 
-        alt={card.id} 
-        className={cardStyle} 
-        style={{animationDelay: delay}}
+    <div id='card' className={containerStyle} style={{ animationDelay: delay }}>
+      <img
+        key={card.id}
+        src={card.images.large}
+        alt={card.id}
+        className={cardStyle}
+        style={{ animationDelay: delay }}
         width='380px'
         height='519.5px'
-        />
+      />
     </div>
   )
 }
